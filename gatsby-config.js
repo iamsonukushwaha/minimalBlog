da42cc52615fc8d07d644bb1eb2d664e1e145d0b
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2022-06-05 13:04:53 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
           },
           {
             name: `Twitter`,
-            url: `https://twitter.com/sonukumarkush12`,
+            url: `https://twitter.com/iamsonukushwaha`,
           },
         ],
       },
